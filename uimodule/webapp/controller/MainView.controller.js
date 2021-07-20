771e0ae7d0ff067ffe5cc0d66aca1c5dbb2d2ad2
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-07-20 14:42:00 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/infocus/PMSReport/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.infocus.PMSReport.controller.MainView", {});
+});
