eb343eb03d12c6a55d5bed2370e00ad58a23f609
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-07-20 17:13:13 +0530

    Basic Page Design for PDV View implemented.

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,5 +1,19 @@
-sap.ui.define(["com/infocus/PMSReport/controller/BaseController"], function (Controller) {
-    "use strict";
+sap.ui.define([
+	"com/infocus/PMSReport/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
+	"use strict";
 
-    return Controller.extend("com.infocus.PMSReport.controller.MainView", {});
+	return Controller.extend("com.infocus.PMSReport.controller.MainView", {
+		onInit: function() {
+			this._sValidPath = sap.ui.require.toUrl("sap/m/sample/PDFViewerEmbedded/sample.pdf");
+			this._sInvalidPath = sap.ui.require.toUrl("sap/m/sample/PDFViewerEmbedded/sample_nonexisting.pdf");
+			this._oModel = new JSONModel({
+				Source: this._sValidPath,
+				Title: "My Custom Title",
+				Height: "600px"
+			});
+			this.getView().setModel(this._oModel);
+		}
+	});
 });
